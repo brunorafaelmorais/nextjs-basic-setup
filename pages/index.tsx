999eaef1b89a52fde77ddcb0149780966c88d250
bruno.morais <bruno.morais@nodo.cc>
2021-02-02 08:43:33 -0300

    basic setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello Next.js</h1>
+      </main>
+    </div>
+  )
+}
